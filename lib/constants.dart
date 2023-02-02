4efca1b696bc92aeab23c10f468da611649f525d
doyon <doyon3x3@gmail.com>
2023-02-03 04:45:49 +0900

    add screenCheck() & margin color value

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,56 +1,74 @@
 import 'package:flutter/material.dart';
 import 'Button.dart';
 
+//스크린 크기에 따른 상수 전달 함수
+screenCheck(BuildContext context) {
+  if (MediaQuery.of(context).size.width >= 700) {
+    return 500;
+  } else {
+    return null;
+  }
+}
+
+//여백 컬러
+const materialColor = Color(0xfffbfbfb);
+
 //자주 사용하는 텍스트, 위젯 스타일을 한 번에 관리
-const kLargeTextStyle =
-    TextStyle(
-      color: Colors.black, fontWeight: FontWeight.w700,
-      fontSize: 18.0, fontFamily: 'hyemin');
+//text styles
+const kLargeTextStyle = TextStyle(
+    color: Colors.black,
+    fontWeight: FontWeight.w700,
+    fontSize: 18.0,
+    fontFamily: 'hyemin');
 
-const kMediumTextStyle =
-    TextStyle(
-      color: Colors.black, fontWeight : FontWeight.w400,
-        fontSize: 16.0, fontFamily: 'hyemin');
+const kMediumTextStyle = TextStyle(
+    color: Colors.black,
+    fontWeight: FontWeight.w400,
+    fontSize: 16.0,
+    fontFamily: 'hyemin');
 
-const kMediumBTextStyle =
-    TextStyle(
-        color: Colors.black, fontWeight : FontWeight.w700,
-        fontSize: 16.0, fontFamily: 'hyemin');
+const kMediumBTextStyle = TextStyle(
+    color: Colors.black,
+    fontWeight: FontWeight.w700,
+    fontSize: 16.0,
+    fontFamily: 'hyemin');
 
-const kSmallTextStyle =
-    TextStyle(
-        color: Colors.black, fontWeight: FontWeight.w400,
-        fontSize: 14.0, fontFamily: 'hyemin');
+const kSmallTextStyle = TextStyle(
+    color: Colors.black,
+    fontWeight: FontWeight.w400,
+    fontSize: 14.0,
+    fontFamily: 'hyemin');
 
-const kTagTextStyle =
-    TextStyle(
-        color: Colors.grey, fontWeight: FontWeight.w400,
-        fontSize: 12.0, fontFamily: 'hyemin');
+const kTagTextStyle = TextStyle(
+    color: Colors.grey,
+    fontWeight: FontWeight.w400,
+    fontSize: 12.0,
+    fontFamily: 'hyemin');
 
-const kButtonTextStyle =
-    TextStyle(
-        color: Colors.grey, fontWeight: FontWeight.w400,
-        fontSize: 10.0, fontFamily: 'hyemin');
+const kButtonTextStyle = TextStyle(
+    color: Colors.grey,
+    fontWeight: FontWeight.w400,
+    fontSize: 10.0,
+    fontFamily: 'hyemin');
 
-var kContainerStyle =
-    BoxDecoration(
-      borderRadius: BorderRadius.circular(30),
-      color: const Color(0xffe4eee9)
-    );
+//widget styles
+//연한 초록 컨테이너
+var kContainerStyle = BoxDecoration(
+    borderRadius: BorderRadius.circular(30), color: const Color(0xffe4eee9));
 
-var kButtonStyle =
-    BoxDecoration(
-        borderRadius: BorderRadius.circular(30),
-        color: const Color(0xfffbefc3)
-    );
+//연한 노랑 컨테이너(버튼)
+var kButtonStyle = BoxDecoration(
+    borderRadius: BorderRadius.circular(30), color: const Color(0xfffbefc3));
 
+//진한 노랑 컨테이너(버튼)
 var kButtonStyleB = BoxDecoration(
     borderRadius: BorderRadius.circular(30), color: const Color(0xffFFD440));
 
-var kCheckBoxStyle = BoxDecoration(
-  border: Border.all(color: Colors.black, width: 2.3)
-);
+//ReservationPage의 체크박스 스타일(검은 테두리, 흰 배경)
+var kCheckBoxStyle =
+    BoxDecoration(border: Border.all(color: Colors.black, width: 2.3));
 
+//함수
 //container with green background
 Widget buildContainer(String text1, String img, String text2) {
   return Container(
@@ -65,7 +83,9 @@ Widget buildContainer(String text1, String img, String text2) {
           height: 60,
           fit: BoxFit.fill,
         ),
-        const SizedBox(height: 20,),
+        const SizedBox(
+          height: 20,
+        ),
         Text(
           text2,
           style: kLargeTextStyle,
@@ -75,7 +95,8 @@ Widget buildContainer(String text1, String img, String text2) {
 
 // purpose 값 전달 기능 => 미구현
 String purpose = '';
-// light yellow button with text : move to another page
+
+// 텍스트 포함 버튼 생성 함수 (연한 노랑)
 Widget buildButton(BuildContext currentPage, Widget linkedPage, String text) {
   return InkWell(
     onTap: () {
@@ -95,14 +116,20 @@ Widget buildButton(BuildContext currentPage, Widget linkedPage, String text) {
   );
 }
 
+//사진 버튼 생성 함수 for sharing with SNS, 임시
 Widget buildImgButton(String imgFile) {
   return InkWell(
     onTap: null,
-    child: Image.asset(imgFile, width: 53.0, height: 53.0,),
+    child: Image.asset(
+      imgFile,
+      width: 53.0,
+      height: 53.0,
+    ),
   );
 }
 
-//linear progress bar, texts with padding
+//Type Test에서 공통되는 상단 컨테이너 생성 함수
+// linear progress bar, texts with padding
 Widget buildTop(int pageNum, double progressValue, String category) {
   return Column(
     children: [
@@ -159,7 +186,7 @@ Widget buildTop(int pageNum, double progressValue, String category) {
   );
 }
 
-//build three buttons
+//build three buttons 함수
 Widget buildButtons(Buttons btn, func) {
   double btnSize = 30;
   double btnSize2 = 40;
@@ -171,32 +198,53 @@ Widget buildButtons(Buttons btn, func) {
       children: [
         Column(
           children: [
-            const SizedBox(height: 5.0,),
-            InkWell(onTap: () {func(1, btn);},
+            const SizedBox(
+              height: 5.0,
+            ),
+            InkWell(
+              onTap: () {
+                func(1, btn);
+              },
               child: Image.asset(
                 btn.imgFile1,
                 width: btnSize,
                 height: btnSize,
               ),
             ),
-            const SizedBox(height: 5.0,),
-            const Text('싫어요', style: kButtonTextStyle,),
+            const SizedBox(
+              height: 5.0,
+            ),
+            const Text(
+              '싫어요',
+              style: kButtonTextStyle,
+            ),
           ],
         ),
-        const SizedBox(width: 30.0,),
+        const SizedBox(
+          width: 30.0,
+        ),
         Column(
           children: [
-            const SizedBox(height: 5.0,),
+            const SizedBox(
+              height: 5.0,
+            ),
             InkWell(
-              onTap: () {func(2, btn);},
+              onTap: () {
+                func(2, btn);
+              },
               child: Image.asset(
                 btn.imgFile2,
                 width: btnSize,
                 height: btnSize,
               ),
             ),
-            const SizedBox(height: 5.0,),
-            const Text('좋아요', style: kButtonTextStyle,)
+            const SizedBox(
+              height: 5.0,
+            ),
+            const Text(
+              '좋아요',
+              style: kButtonTextStyle,
+            )
           ],
         ),
         const SizedBox(
@@ -205,14 +253,19 @@ Widget buildButtons(Buttons btn, func) {
         Column(
           children: [
             InkWell(
-              onTap: () {func(3, btn);},
+              onTap: () {
+                func(3, btn);
+              },
               child: Image.asset(
                 btn.imgFile3,
                 width: btnSize2,
                 height: btnSize2,
               ),
             ),
-            const Text('최고예요!', style: kButtonTextStyle,)
+            const Text(
+              '최고예요!',
+              style: kButtonTextStyle,
+            )
           ],
         )
       ],
@@ -220,21 +273,34 @@ Widget buildButtons(Buttons btn, func) {
   );
 }
 
-//topping
+//Type test : topping 사진 생성 함수
 Widget toppingImg(String menuName, String imgFile, String menuInfo) {
   return Container(
     width: 230.0,
-    padding: const EdgeInsets.only(
-        left: 25.0, top:10.0, bottom: 10.0, right: 25.0),
-    child: (
-        Column(
-          children: [
-            Text(menuName, style: kMediumBTextStyle,),
-            const SizedBox(height: 5.0,),
-            Image.asset(imgFile, width: 145, height: 61,),
-            const SizedBox(height: 5.0,),
-            Text(menuInfo, style: kTagTextStyle,)
-          ],
+    padding:
+        const EdgeInsets.only(left: 25.0, top: 10.0, bottom: 10.0, right: 25.0),
+    child: (Column(
+      children: [
+        Text(
+          menuName,
+          style: kMediumBTextStyle,
+        ),
+        const SizedBox(
+          height: 5.0,
+        ),
+        Image.asset(
+          imgFile,
+          width: 145,
+          height: 61,
+        ),
+        const SizedBox(
+          height: 5.0,
+        ),
+        Text(
+          menuInfo,
+          style: kTagTextStyle,
         )
-    ),);
+      ],
+    )),
+  );
 }
